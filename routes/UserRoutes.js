ca54747d67cd0176fd452d2272cca69ce1ba5d72
danyentezari <dany.entezari@gmail.com>
2019-11-09 15:56:50 +0400

    Added passport for authentication.

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
 const UserModel = require('../models/UserModel');
+const secret = 'thisIsTheSecret';
 
 router.post('/register', (req, res)=>{
 
@@ -54,6 +56,66 @@ router.post('/register', (req, res)=>{
             }
         )
     });
+});
+
+router.post('/login', (req, res)=>{
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Step 1. Check to see if account with specified email exists
+    UserModel
+    .findOne({ email: email }) //{}
+    .then((theUser)=>{
+
+        // If an account exists...
+        if(theUser) {
+            
+
+            // Step 2. Compare the submitted password with the encrypted
+            // password in database
+            bcrypt
+            .compare(password, theUser.password)
+            .then((isMatch)=>{
+
+                // If password correct...
+                if(isMatch) {
+
+                    // Step 3. Decide the payload (encrypted data you want to send back to client)
+                    const payload = {
+                        id: theUser.id,
+                        email: theUser.email
+                    }
+
+                    // Step 4. Generate the JSON Web Token
+                    jwt.sign(
+                        payload,
+                        secret,
+                        (err, theJWT)=>{
+
+                            // Step 5. Send the JWT to the client
+                            res.json({ token: theJWT })
+                        }
+                    )
+
+                } 
+                // Otherwise, if password is not correct.
+                else {
+                    res.json({ message: 'Wrong password' })
+                }
+            })
+            .catch()
+
+
+        } 
+        
+        // Otherwise, if account doesn't exist
+        else {
+            res.json({ message: "No user with this account exists" })
+        }
+    })
+    .catch()
+
 });
 
 
